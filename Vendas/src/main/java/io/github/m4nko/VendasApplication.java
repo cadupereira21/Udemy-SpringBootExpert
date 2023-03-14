907233dc88b97cf8984147e3f7a117fff81b9c26
Carlos Pereira <carlospereira@ciandt.com>
2023-03-14 15:22:31 -0300

    Component scan notation

--- Vendas/src/main/java/io/github/m4nko/VendasApplication.java
+++ Vendas/src/main/java/io/github/m4nko/VendasApplication.java
@@ -4,10 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+/*
+@ComponentScan(basePackages = {
+        "io.github.m4nko.config",
+        "io.github.m4nko.controller"
+        }) // Anotação para que o Spring Boot mantenha tracking dos nossos pacotes do MVC (Repository, Service, Controller) no container
+           // OBS: Spring boot já mantem o tracking de todos os pacotes e classes a partir do pacote em que está definido o "@SpringBootApplication"*/
 @RestController
 public class VendasApplication {
 
