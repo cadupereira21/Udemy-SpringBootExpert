77921896e7a7853a2e9eff678d64fce7379fd54a
Carlos Pereira <carlospereira@ciandt.com>
2023-03-15 10:32:56 -0300

    Create: ClienteReṕsitory with jdbcTemplate for querys

--- Vendas/src/main/java/io/github/m4nko/VendasApplication.java
+++ Vendas/src/main/java/io/github/m4nko/VendasApplication.java
@@ -1,12 +1,33 @@
 package io.github.m4nko;
 
+import io.github.m4nko.model.Cliente;
+import io.github.m4nko.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.ref.Cleaner;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired ClienteRepository clienteRepository){
+        return args -> {
+            Cliente cliente = new Cliente("Carlos");
+            clienteRepository.Salvar(cliente);
+            Cliente cliente2 = new Cliente("Clara");
+            clienteRepository.Salvar(cliente2);
+
+            List<Cliente> todosClientes = clienteRepository.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
---
+++ Vendas/src/main/java/io/github/m4nko/repository/ClienteRepository.java
@@ -0,0 +1,34 @@
+package io.github.m4nko.repository;
+
+import io.github.m4nko.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class ClienteRepository {
+    private static String INSERT = "insert into cliente (nome) values (?)";
+    private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    public Cliente Salvar(Cliente cliente){
+        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        return cliente;
+    }
+
+    public List<Cliente> obterTodos(){
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+                Integer id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                return new Cliente(id, nome);
+            }
+        });
+    }
+}
