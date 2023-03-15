4b8890472244f4481b3df46d762eb39c2e6c7da9
Carlos Pereira <carlospereira@ciandt.com>
2023-03-15 09:47:50 -0300

    Configure: preparing all needed modules, drivers and configurations

--- Vendas/src/main/java/io/github/m4nko/VendasApplication.java
+++ Vendas/src/main/java/io/github/m4nko/VendasApplication.java
@@ -1,35 +1,12 @@
 package io.github.m4nko;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-/*
-@ComponentScan(basePackages = {
-        "io.github.m4nko.config",
-        "io.github.m4nko.controller"
-        }) // Anotação para que o Spring Boot mantenha tracking dos nossos pacotes do MVC (Repository, Service, Controller) no container
-           // OBS: Spring boot já mantem o tracking de todos os pacotes e classes a partir do pacote em que está definido o "@SpringBootApplication"*/
 @RestController
 public class VendasApplication {
-
-//    @Autowired
-//    @Qualifier("appName")
-//    private String appName; // Variável referente à configuração na classe AppConfiguration -> appName()
-
-    @Value("${application.name}") // Notação busca arquivo de propriedades application.properties e injeta o valor de application.name na variavel appName
-    private String appName;
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return appName;
-    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
