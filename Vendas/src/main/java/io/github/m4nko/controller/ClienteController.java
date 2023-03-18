d07ab4ac083b41ae1557942be1d1596f27b7b0d4
Carlos Pereira <carlospereira@ciandt.com>
2023-03-18 09:10:37 -0300

    Update: ClienteController now search and returns a Cliente object in a JSON

--- Vendas/src/main/java/io/github/m4nko/controller/ClienteController.java
+++ Vendas/src/main/java/io/github/m4nko/controller/ClienteController.java
@@ -1,21 +1,33 @@
 package io.github.m4nko.controller;
 
 import io.github.m4nko.model.Cliente;
+import io.github.m4nko.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
-    @RequestMapping(
-            value = {"/hello/{nome}", "/api/hello"}, // Recebe array de strings para definição de várias rotas
-            method = RequestMethod.GET,
-            consumes = {"application/json", "application/xml"}, // Tipo de dados recebidos / esperados
-            produces = {"application/json", "application/xml"} // Tipo de dados enviados
-    )
 
-    @ResponseBody
-    public String helloClientes(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){ // Parâmetro com anotação @RequestBody diz ao Spring para esperar no corpo da requisição um objeto Cliente (no formato JSON ou XML conforme parâmetro consume)
-        return String.format("Hello %s", nomeCliente);
+    private ClienteRepository clienteRepository;
+
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @GetMapping("/{id}") // Equivalente ao @RequestMapping com método get
+    //@ResponseBody
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id){ // Parâmetro com anotação @RequestBody diz ao Spring para esperar no corpo da requisição um objeto Cliente (no formato JSON ou XML conforme parâmetro consume)
+        Optional<Cliente> result = clienteRepository.findById(id);
+
+        if(result.isPresent()) return ResponseEntity.ok(result.get());
+
+        return ResponseEntity.notFound().build();
     }
 }
+
+
