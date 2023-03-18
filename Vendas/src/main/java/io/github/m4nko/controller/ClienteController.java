6ec4842a668d8230fc4c61a523f36a976f5f7bd4
Carlos Pereira <carlospereira@ciandt.com>
2023-03-18 08:55:09 -0300

    Update: ClienteController new parameters

--- Vendas/src/main/java/io/github/m4nko/controller/ClienteController.java
+++ Vendas/src/main/java/io/github/m4nko/controller/ClienteController.java
@@ -1,17 +1,21 @@
 package io.github.m4nko.controller;
 
+import io.github.m4nko.model.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/hello/{nome}", "/api/hello"}, // Recebe array de strings para definição de várias rotas
+            method = RequestMethod.GET,
+            consumes = {"application/json", "application/xml"}, // Tipo de dados recebidos / esperados
+            produces = {"application/json", "application/xml"} // Tipo de dados enviados
+    )
+
     @ResponseBody
-    public String helloClientes(@PathVariable("nome") String nomeCliente){
+    public String helloClientes(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){ // Parâmetro com anotação @RequestBody diz ao Spring para esperar no corpo da requisição um objeto Cliente (no formato JSON ou XML conforme parâmetro consume)
         return String.format("Hello %s", nomeCliente);
     }
 }
