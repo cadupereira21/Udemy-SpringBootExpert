c5cb55056c2e0a6a5a05af68f905687d4dc55911
Carlos Pereira <carlospereira@ciandt.com>
2023-04-26 11:30:04 -0300

    Update: changed autorizations on SecurityConfiguration

--- Vendas/src/main/java/io/github/m4nko/config/SecurityConfig.java
+++ Vendas/src/main/java/io/github/m4nko/config/SecurityConfig.java
@@ -30,9 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-                .antMatchers("api/clientes/**").hasRole("USER")
+                .antMatchers("api/clientes/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers("api/pedidos/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("api/produtos/**").hasRole("ADMIN")
-                .antMatchers("api/pedidos/**").hasRole("USER")
             .and()
                 .formLogin();
     }
