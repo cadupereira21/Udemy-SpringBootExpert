5929a5d0fe14015292552c27472b0bace6d5a483
Carlos Pereira <carlospereira@ciandt.com>
2023-04-27 10:01:50 -0300

    Create: implemented security config for endpoint api/usuarios post

--- Vendas/src/main/java/io/github/m4nko/config/SecurityConfig.java
+++ Vendas/src/main/java/io/github/m4nko/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.github.m4nko.config;
 import io.github.m4nko.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("api/clientes/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("api/pedidos/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("api/produtos/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "api/usuarios/**").permitAll()
             .and()
                 .httpBasic();
     }
